29b7bd63b6fe77e837d1be1b814af97c765fbe02
GasbroNinja <gasbroider@live.it>
2023-03-31 16:42:45 +0200

    Commit Project app React Netflix U3-W1-D4/5

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import MyNav from "./components/MyNav";
+import MyHomePage from "./components/MyHomePage";
+
+
+function App() {
+  return (
+    <>
+    <MyNav />
+    <MyHomePage />
+    
+
+
+    
+    </>
+  );
+}
+
+export default App;
